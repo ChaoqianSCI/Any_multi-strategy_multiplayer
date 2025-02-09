2209c47dd18334ffb13937632d3e29aa6ccc887c
Chaoqian Wang <126829215+ChaoqianSCI@users.noreply.github.com>
2025-02-09 16:21:56 +0800

    Delete pool punishment directory

--- pool punishment/events1.m
+++
@@ -1,6 +0,0 @@
-function [value,isterminal,direction] = events1(t,y)
-global dx;
-value = sum(abs(dx(:)))-0.0001;
-isterminal= 1;
-direction = 0;
-end
--- pool punishment/events2.m
+++
@@ -1,6 +0,0 @@
-function [value,isterminal,direction] = events2(t,y)
-global dx;
-value = sum(abs(dx(:)))-0.0000001;
-isterminal= 1;
-direction = 0;
-end
